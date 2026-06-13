a2e6d08b15f0f9476df61e2ea436a627b75df7de
Yoni Rosen <yoni.rosen-ctr@govini.com>
2026-06-13 16:07:16 -0400

    Oh well

--- IsSAbel/checkSN.py
+++ IsSAbel/checkSN.py
@@ -23,14 +23,36 @@ def applyPerm(base, perm):
         permutate.append(base[perm[i]-1])
     return permutate
 
-def printPerm(s_size):
+def isSameCyclic(a, b):
+    if len(a) != len(b):
+        return False
+    for i in range(0, len(b)):
+        if a == b:
+            return True
+        a = [a[len(a)-1]] + a[0:-1]
+    return False
+
+def isAbelicWithinCycle(s_size):
     base = [chr(96+n) for n in range(1,s_size+1)]
     print(s_size)
     print(base)
     allS = getAllPerm(s_size)
 #    print(allS)
-    for perm in allS:
-        print(applyPerm(base, perm))
+    for i in range(0, len(allS)):
+        for j in range(i+1, len(allS)):
+            jXi = applyPerm(applyPerm(base, allS[i]), allS[j])
+            iXj = applyPerm(applyPerm(base, allS[j]), allS[i])
+            if not isSameCyclic(iXj, jXi):
+                print(f' i:{allS[i]}, j:{allS[j]} => jXi:{jXi}, iXj={iXj}')
+                return False
+    return True
+
+def printPerm(s_size):
+    isAbel = isAbelicWithinCycle(s_size)
+    print(f' S{s_size} ==> {isAbel}')
+        
+#    for perm in allS:
+#        print(applyPerm(base, perm))
 
 
 if __name__ == '__main__':
